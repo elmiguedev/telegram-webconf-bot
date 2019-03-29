1c3472b3bec3486b27c291d81569629479a7c1d7
name <mmoyano16@gmail.com>
2019-03-29 20:25:46 -0300

    cambios en los bots

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const app = express()
 // define bot commands
 bot.on('/entradas', async function(msg){
 
-    const data = await axios.get('https://checkout.webconf.tech/api/hypes?include=y-eia');
-    const entradas = data[0].attributes;
-    const mensaje = `Cantidad de entradas vendidas: ${entradas.ticketsSold} !! :D `;
+    // const data = await axios.get('https://checkout.webconf.tech/api/hypes?include=y-eia');
+    // const entradas = data[0].attributes;
+    // const mensaje = `Cantidad de entradas vendidas: ${entradas.ticketsSold} !! :D `;
 
-    msg.reply.text(mensaje);
+    // msg.reply.text(mensaje);
+    msg.reply.text('test');
 
 });
 
