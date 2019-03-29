ef29a0cc9cfff6dd0cad8ae1d4c6d52086eb8ae7
name <mmoyano16@gmail.com>
2019-03-29 20:21:57 -0300

    fix bot

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ const Telebot = require('telebot');
 const bot = new Telebot(process.env.TOKEN);
 const app = express()
 
+// define bot commands
+bot.on('/entradas', async function(msg){
+
+    const data = await axios.get('https://checkout.webconf.tech/api/hypes?include=y-eia');
+    const entradas = data[0].attributes;
+    const mensaje = `Cantidad de entradas vendidas: ${entradas.ticketsSold} !! :D `;
+
+    msg.reply.text(mensaje);
+    
+});
+
 // add some security-related headers to the response
-//bot.start();
 app.use(helmet())
-
-app.get('/', (req, res) => {
-    res.send('webconf bot!! :D')
-})
-
+app.get('/', (req, res) => {res.send('webconf bot!! :D')});
 app.listen(process.env.PORT || 8080,function() {
     console.log('iniciando express');
     bot.start();
 });
-
 
 
 //bot.on('/taunts', function(msg){
