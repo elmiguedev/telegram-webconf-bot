3487b45257d2cb1c0c5d8bcad538f428530a99d2
name <mmoyano16@gmail.com>
2019-03-29 19:43:22 -0300

    test bot

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const helmet = require('helmet')
 const axios = require('axios');
 const Telebot = require('telebot');
 
-const bot = new Telebot(process.env.TOKEN);
+//const bot = new Telebot(process.env.TOKEN);
 const app = express()
 
 // add some security-related headers to the response
-bot.start();
+//bot.start();
 app.use(helmet())
 
 app.get('/', (req, res) => {
