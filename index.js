46ec6108d35e390fa03b16cd805d0aff9ce32c11
name <mmoyano16@gmail.com>
2019-03-29 19:38:47 -0300

    test bot

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const helmet = require('helmet')
+const axios = require('axios');
+const Telebot = require('telebot');
 
+const bot = new Telebot(process.env.TOKEN);
 const app = express()
 
 // add some security-related headers to the response
+bot.start();
 app.use(helmet())
 
-app.get('*', (req, res) => {
-    res.set('Content-Type', 'text/html')
-    res.send(200, `
-        <h1><marquee direction=right>Y EEIAA??? '/' on Now 2.0!</marquee></h1>
-    `)
+app.get('/', (req, res) => {
+    res.send('webconf bot!! :D')
 })
 
 module.exports = app
+
+
+//bot.on('/taunts', function(msg){
+//  return msg.reply.text(responses.taunts);
+//});
