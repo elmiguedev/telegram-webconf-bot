02c042402d08356c3c954bc69d367a3614be77c0
name <mmoyano16@gmail.com>
2019-03-29 20:31:44 -0300

    cambios en los bots 2

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ const bot = new Telebot(process.env.TOKEN);
 const app = express()
 
 // define bot commands
-bot.on('/entradas', async (msg) => {
+bot.on('/entradas', (msg) => {
 
 
-    // const data = await axios.get('https://checkout.webconf.tech/api/hypes?include=y-eia');
-    // const entradas = data[0].attributes;
-    // const mensaje = `Cantidad de entradas vendidas: ${entradas.ticketsSold} !! :D `;
+    axios.get('https://checkout.webconf.tech/api/hypes?include=y-eia').then(
+        res => {
+            const entradas = res.data.data[0].attributes;
+            // const mensaje = `Cantidad de entradas vendidas: ${entradas.ticketsSold} !! :D `;
+            return msg.reply.text('test');
+            
+        }
+    );
 
     // msg.reply.text(mensaje);
-    return msg.reply.text('test');
 
 });
 
