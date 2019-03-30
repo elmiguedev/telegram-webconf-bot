913e712522185877f0bc2308ce1edd9be8145ec1
name <mmoyano16@gmail.com>
2019-03-29 22:04:30 -0300

    now again

--- index.js
+++ index.js
@@ -7,27 +7,32 @@ const app = express();
 
 // define bot commands
 // bot.on('/entradas', (msg) => {
+
+
+//     axios.get('https://checkout.webconf.tech/api/hypes?include=y-eia').then(
+//         res => {
+//             const entradas = res.data.data[0].attributes;
+//             // const mensaje = `Cantidad de entradas vendidas: ${entradas.ticketsSold} !! :D `;
+//             return msg.reply.text('test');
+
+//         }
+//     );
+
+//     // msg.reply.text(mensaje);
+
+// });
+
+// add some security-related headers to the response
+app.use(helmet())
+app.get('/', (req, res) => { res.send('webconf bot!! :D') });
+app.listen(process.env.PORT || 8080, function () {
+    const bot = new TeleBot(process.env.TOKEN);
+    bot.on('/entradas', (msg) => {
+        return msg.reply.text('y eia');
+    });
     
     
-    //     axios.get('https://checkout.webconf.tech/api/hypes?include=y-eia').then(
-        //         res => {
-            //             const entradas = res.data.data[0].attributes;
-            //             // const mensaje = `Cantidad de entradas vendidas: ${entradas.ticketsSold} !! :D `;
-            //             return msg.reply.text('test');
-            
-            //         }
-            //     );
-            
-            //     // msg.reply.text(mensaje);
-            
-            // });
-            
-            // add some security-related headers to the response
-            app.use(helmet())
-            app.get('/', (req, res) => {res.send('webconf bot!! :D')});
-            app.listen(process.env.PORT || 8080, function() {
-                const bot = new TeleBot(process.env.TOKEN);
-                //   bot.start();
+    bot.start();
 });
 
 //module.exports = app;
