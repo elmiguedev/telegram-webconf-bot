b59b2af45583bc5a33d34294bd7239572565c4c3
name <mmoyano16@gmail.com>
2019-03-29 21:24:33 -0300

    now again

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const express = require('express')
 const helmet = require('helmet')
 const axios = require('axios');
-const Telebot = require('telebot');
+// const Telebot = require('telebot');
 
-const bot = new Telebot(process.env.TOKEN);
+// const bot = new Telebot(process.env.TOKEN);
 const app = express()
 
 // define bot commands
-bot.on('/entradas', (msg) => {
+// bot.on('/entradas', (msg) => {
 
 
-    axios.get('https://checkout.webconf.tech/api/hypes?include=y-eia').then(
-        res => {
-            const entradas = res.data.data[0].attributes;
-            // const mensaje = `Cantidad de entradas vendidas: ${entradas.ticketsSold} !! :D `;
-            return msg.reply.text('test');
+//     axios.get('https://checkout.webconf.tech/api/hypes?include=y-eia').then(
+//         res => {
+//             const entradas = res.data.data[0].attributes;
+//             // const mensaje = `Cantidad de entradas vendidas: ${entradas.ticketsSold} !! :D `;
+//             return msg.reply.text('test');
             
-        }
-    );
+//         }
+//     );
 
-    // msg.reply.text(mensaje);
+//     // msg.reply.text(mensaje);
 
-});
+// });
 
 // add some security-related headers to the response
 app.use(helmet())
 app.get('/', (req, res) => {res.send('webconf bot!! :D')});
 app.listen(process.env.PORT || 8080);
-bot.start();
+// bot.start();
 
 //bot.on('/taunts', function(msg){
 //  return msg.reply.text(responses.taunts);
