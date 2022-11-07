09d9e3c990d9f406526cb5ea855e54bb8beab54a
Migue Moyano <miguel.moyano@etermax.com>
2022-11-07 08:30:55 -0300

    summary fix

--- src/services/WebConfApi/TicketsSummary.js
+++ src/services/WebConfApi/TicketsSummary.js
@@ -1,9 +1,9 @@
 class TicketsSummary {
   constructor(tickets) {
-    this.reserved = tickets.find(t => t.state === "reserved").count;
-    this.paymentPending = tickets.find(t => t.state === "payment_pending").count;
-    this.open = tickets.find(t => t.state === "open").count;
-    this.pending = tickets.find(t => t.state === "pending").count;
+    this.reserved = tickets.find(t => t.state === "reserved")?.count || 0;
+    this.paymentPending = tickets.find(t => t.state === "payment_pending")?.count || 0;
+    this.open = tickets.find(t => t.state === "open")?.count || 0;
+    this.pending = tickets.find(t => t.state === "pending")?.count || 0;
   }
 }
 
